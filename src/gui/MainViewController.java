c437b315c8898fe560afb40aae6446738bf02b0d
CrisRocha01 <cristiano_g_rocha@yahoo.com.br>
2021-04-09 20:42:00 -0300

    DepartmentList view design

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -32,12 +32,12 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		System.out.println("onMenuItemDepartmentAction");
+		loadView("/gui/DepartmentList.fxml");;
 	}
 
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml"); //absoluteName
 	}
 
 	@Override
@@ -45,18 +45,27 @@ public class MainViewController implements Initializable {
 		// TODO Auto-generated method stub
 
 	}
-
-	private synchronized void loadView(String absoluteName) {// funcão para trocar a view
+	// funcão para trocar a view
+	private synchronized void loadView(String absoluteName) { //synchronized: garante a não interrupção(mult tred)
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
 			VBox newVBox = loader.load();
-
+			// instancia a cena principal
 			Scene mainScene = Main.getMainScene();
+			// getRoot pega o 1o elemento da view
+			// casting para ScrollPane (tipo do 1o elemento da mainView
+			//getContent para acessar o conteúdo do <content>
+			//casting para VBox (tipo do 1o conteudo do <content>
 			VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
 			
+			//guardar referência do menu (que não pode mudar):
+			//getChildren().get(0): pega o 1o elemento do <children>
 			Node mainMenu = mainVbox.getChildren().get(0);
+			//.clear() limpa os elementos do elemento
 			mainVbox.getChildren().clear();
+			//.add() adiciona os elementos da instancia de Node"mainMenu"
 			mainVbox.getChildren().add(mainMenu);
+			//.addAll() adiciona a coleção de elementos de <children> da view instanciada(absoluteName)
 			mainVbox.getChildren().addAll(newVBox.getChildren());
 
 		} catch (IOException e) {
