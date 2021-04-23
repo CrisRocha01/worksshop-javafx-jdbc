02cfc71dc2076b344193bd4855eef09302cdac43
CrisRocha01 <cristiano_g_rocha@yahoo.com.br>
2021-04-23 20:44:04 -0300

    DepartmentService

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,8 +15,11 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.service.DepartmentService;
 
 public class DepartmentListController implements Initializable {
+	
+	private DepartmentService service;
 	
 	@FXML
 	private TableView<Department> TableViewDepartment;
@@ -25,11 +31,17 @@ public class DepartmentListController implements Initializable {
 	private TableColumn<Department, String> TableColumnName;
 	
 	@FXML
-	private Button BtAction; 
+	private Button BtNew; 
+	
+	private ObservableList<Department> obsList;
 		
 	@FXML
-	public void onBtAction() {
-		System.out.println("onBtAction");
+	public void onBtNewAction() {
+		System.out.println("onBtNewAction");
+	}
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 
 	@Override
@@ -49,6 +61,15 @@ public class DepartmentListController implements Initializable {
 		TableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
 		TableViewDepartment.prefWidthProperty().bind(stage.widthProperty());
 		
+	}
+	
+	public void updateTableView() {
+		if (service == null) {
+			throw new IllegalStateException("Service is null");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);
+		TableViewDepartment.setItems(obsList);
 	}
 	
 
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -15,6 +15,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import model.service.DepartmentService;
 
 public class MainViewController implements Initializable {
 
@@ -32,7 +33,7 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView("/gui/DepartmentList.fxml");;
+		loadView2("/gui/DepartmentList.fxml");;
 	}
 
 	@FXML
@@ -67,6 +68,37 @@ public class MainViewController implements Initializable {
 			mainVbox.getChildren().add(mainMenu);
 			//.addAll() adiciona a coleção de elementos de <children> da view instanciada(absoluteName)
 			mainVbox.getChildren().addAll(newVBox.getChildren());
+
+		} catch (IOException e) {
+			Alerts.showAlert("IOException", "Error loading View", e.getMessage(), Alert.AlertType.ERROR);
+		}
+	}
+	
+	private synchronized void loadView2(String absoluteName) { //synchronized: garante a não interrupção(mult tred)
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			// instancia a cena principal
+			Scene mainScene = Main.getMainScene();
+			// getRoot pega o 1o elemento da view
+			// casting para ScrollPane (tipo do 1o elemento da mainView
+			//getContent para acessar o conteúdo do <content>
+			//casting para VBox (tipo do 1o conteudo do <content>
+			VBox mainVbox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			//guardar referência do menu (que não pode mudar):
+			//getChildren().get(0): pega o 1o elemento do <children>
+			Node mainMenu = mainVbox.getChildren().get(0);
+			//.clear() limpa os elementos do elemento
+			mainVbox.getChildren().clear();
+			//.add() adiciona os elementos da instancia de Node"mainMenu"
+			mainVbox.getChildren().add(mainMenu);
+			//.addAll() adiciona a coleção de elementos de <children> da view instanciada(absoluteName)
+			mainVbox.getChildren().addAll(newVBox.getChildren());
+			
+			DepartmentListController controller = loader.getController();
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();
 
 		} catch (IOException e) {
 			Alerts.showAlert("IOException", "Error loading View", e.getMessage(), Alert.AlertType.ERROR);
